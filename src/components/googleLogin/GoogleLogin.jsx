ca5093f641d93a2a29bdde575a3e4f1db990e5f7
artemkuskin <artemkuskin7@gmail.com>
2023-06-27 17:48:27 +0300

    fix

--- src/components/googleLogin/GoogleLogin.jsx
+++ src/components/googleLogin/GoogleLogin.jsx
@@ -6,7 +6,7 @@ const Login = (setIsAuth) => {
     console.log(resp);
   };
   return (
-    <GoogleOAuthProvider clientId="910888150173-qmfv074i31cd9fcv571u5t1fnrrsf69a.apps.googleusercontent.com">
+    <GoogleOAuthProvider>
       <GoogleLogin
         onSuccess={asd}
         onError={() => {
